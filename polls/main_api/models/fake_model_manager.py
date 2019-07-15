993ca05997499dd0d731d461b0cf380a6bd39f25
a.baronskiy <a.baronskiy@mail.ru>
2019-07-15 10:39:27 +0300

    поправил документацию к апи, изменил модели на товары

--- polls/main_api/models/fake_model_manager.py
+++ polls/main_api/models/fake_model_manager.py
@@ -11,43 +11,20 @@ class FakeModelManager:
     def __init__(self):
         self.min_insts = 1
         self.max_insts = 10
-        self.min_price = 300
+        self.min_price = 50
         self.max_price = 100000
 
-        self.max_id = 1000
-
-    def _create_list_fake_insts(self):
-        count = random.randint(self.min_insts, self.max_insts)
-        insts = []
-        idx = set()
-
+    async def _create_fake_products(self, count):
+        products = []
         for i in range(count):
-            price = random.uniform(self.min_price, self.max_price)
+            product = api.create_product(fake.name(), fake.text(), \
+             random.uniform(self.min_price, self.max_price), fake.url())
+            products.append(product)
 
-            _id = 0
-            while True:
-                _id = random.randint(1, self.max_id)
-                if _id not in idx:
-                    idx.add(_id)
-                    break
-
-            inst = api.create_instance(_id, fake.word(), fake.word(), price, fake.word())
-            insts.append(inst)
-
-        return insts
-
-
-    async def _create_fake_events(self, count):
-        events = []
-        for i in range(count):
-            insts = self._create_list_fake_insts()
-            event = api.create_event(fake.name(), fake.text(), insts)
-            events.append(event)
-
-        await db.conn.test_collection.insert_many(events)
+        await db.conn.test_collection.insert_many(products)
 
             
-# m = ModelManager()
+m = FakeModelManager()
 
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(m._create_fake_events(10))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(m._create_fake_products(10))
