80562f0a63ccf9f1f95284444778d1633e5145ba
a.baronskiy <a.baronskiy@mail.ru>
2019-07-16 14:41:59 +0300

    менеджер моделей main_app реализован

--- polls/models/model_manager.py
+++ polls/models/model_manager.py
@@ -9,7 +9,10 @@ def add_users_intention_query(uid, pid, dest_id):
     return {'_id' : ObjectId(uid)}, {'$addToSet' : {'intentions' : {'p_id' :  ObjectId(pid), 'dest_id' : ObjectId(dest_id)}}}
 
 def del_users_intention_query(uid, pid, dest_id)
-    return {'_id' : ObjectId(uid)}, {'$pull' : {'wishes' : {'p_id' : ObjectId(pid), 'dest_id' : ObjectId(dest_id)}}}
+    return {'_id' : ObjectId(uid)}, {'$pull' : {'intentions' : {'p_id' : ObjectId(pid), 'dest_id' : ObjectId(dest_id)}}}
+
+def del_users_wish_query(uid, pid)
+    return {'_id' : ObjectId(uid)}, {'$pull' : {'wishes' : {'p_id' : ObjectId(pid)}}}
 
 def reserve_users_wish_query(uid, pid, dest_id):
     return {'_id': ObjectId(dest_id), 'wishes.p_id' : ObjectId(pid)}, {'$set' : {'wishes.$.reserved' : True, 'wishes.$.sponsor_id' : ObjectId(uid)}}
@@ -19,10 +22,16 @@ def close_res_users_wish_query(uid, pid, dest_id):
     return {'_id': ObjectId(dest_id), 'wishes.p_id' : ObjectId(pid)}, {'$set' : {'wishes.$.reserved' : False}
 
 
+
 # TODO реализация транзакций
 class ProfileManager:
     async def check_wish(self, uid, pid):
-        res = await db.profiles_collection.find_one({'_id' : ObjectId(uid)})
+        res = await db.profiles_collection.find_one({'_id' : ObjectId(uid), 'wishes' : {'p_id' : ObjectId(pid)}})
+        if res is None:
+            return False
+        else:
+            return True
+
 
     async def check_user(self, uid):
         res = await db.profiles_collection.find_one({'_id' : ObjectId(uid)})
@@ -31,6 +40,7 @@ class ProfileManager:
         else:
             return True
 
+
     async def check_intention(self, uid, pid, dest_id):
         res = await db.profiles_collection.find_one({'_id' : ObjectId(uid), 'intentions' : {'p_id' : ObjectId(pid), 'dest_id' : ObjectId(dest_id)}})
         if res is None:
@@ -38,12 +48,14 @@ class ProfileManager:
         else:
             return True
 
+
     async def check_product(self, pid):
         res = await db.product_collection.find_one({'_id' : ObjectId(pid)})
         if res is None:
             return False
+        else:
+            return True
 
-        return True
 
     async def add_users_wish(self, uid, pid):
         res = await self.check_product(pid)
@@ -61,12 +73,28 @@ class ProfileManager:
 
         return True
 
+
     async def del_users_wish(self, uid, pid):
         res = await self.check_user(uid)
         if res is None:
             return False
 
-        pass
+        res = await self.check_wish(uid, pid)
+        if res is None:
+            return False
+
+        query = del_users_wish_query(uid, pid)
+        res = await db.profiles_collection.update_one(query)
+        if res is None:
+            return False
+
+        # TODO брать id профиля для быстрого удаления элемента
+        res = await db.profiles_collection.update_one({'intentions' : {'p_id' :  ObjectId(pid), 'dest_id' : ObjectId(uid)})
+        if res is None:
+            return False
+
+        return True
+
 
     async def add_users_intention(self, uid, pid, dest_id):
         res = await self.check_user(uid)
@@ -93,6 +121,7 @@ class ProfileManager:
 
         return True
 
+
     async def del_users_intention(self, uid, pid, dest_id):
         res = await self.check_user(uid)
         if res is None:
@@ -106,7 +135,6 @@ class ProfileManager:
         if res is None:
             return False
 
-        # TODO еще нужно убрать пронирование подарка
         res = await self.check_intention(uid, pid, dest_id)
         if res is None:
             return False
@@ -122,15 +150,4 @@ class ProfileManager:
             return False
 
         return True
-    
-# db.getCollection('profiles').update( {_id : ObjectId("5d2cca26e6f19f24a6a9fd2a")}, {$push : {wishes : ObjectId("5d2c7cf436e8a861bb87a942")}} )
-# db.getCollection('profiles').update( {_id : ObjectId("5d2cca26e6f19f24a6a9fd2a")}, {$pull : {wishes : ObjectId("5d2c7cf436e8a861bb87a942")}} )
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(add_users_wish_queery("5d2cca26e6f19f24a6a9fd2a", "5d2c7cf436e8a861bb87a941"))
-
-# res = reserve_users_wish_query("5d2cca26e6f19f24a6a9fd2a", "5d2c7cf436e8a861bb87a941", "5d2c7cf436e8a861bb87a941")
-# print(type(res), res)
-# condition, update = reserve_users_wish_query("5d2cca26e6f19f24a6a9fd2a", "5d2c7cf436e8a861bb87a941", "5d2c7cf436e8a861bb87a941")
-# print(type(condition), condition)
     
