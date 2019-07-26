4a5c2007fb28fecedf646d441ba237a1dd9aaa83
Surfingbird <a.baronskiy@mail.ru>
2019-07-27 02:00:02 +0300

    написал небольшой клиент для elastick_search

---
+++ polls/main_api_app/elastick_client.py
@@ -0,0 +1,66 @@
+import aiohttp
+import asyncio
+import json
+
+from polls.models.fake_model_manager import FakeModelManager
+
+class ElastickClient:
+    path = 'http://0.0.0.0:9200/products/product/'
+
+    def __init__(self):
+        self.session = None
+        self.last_insert_id = 100
+
+    def connect(self):
+          self.session = aiohttp.ClientSession()
+
+    async def disconnect(self):
+        await self.session.close()
+
+    async def add_product(self, product):
+        data =  product.__dict__.copy()
+        _id = data.pop('_id')
+        data['id'] = _id
+
+        self.last_insert_id += 1
+        url = self.path + str(self.last_insert_id) + '?pretty'
+
+        async with self.session.put(url, json=(data)) as resp:
+            pass
+
+    async def get_products(self, pattern):
+        query = {"query": { "match": { "product_name": pattern } }}
+
+        url = self.path + '_search'
+        result = []
+
+        async with self.session.get(url, json=(query)) as resp:
+            data = await resp.json()
+
+            for node in data['hits']['hits']:
+                product = node['_source']
+                _id = product.pop('id')
+                product['_id'] = _id
+
+                result.append(product)
+
+        return
+
+
+
+
+# es = ElastickClient()
+# fm = FakeModelManager()
+
+# async def main():
+#     es.connect()
+
+#     product = await fm._create_fake_product()
+#     product.product_name = 'lol'
+#     await product.save()
+
+#     await es.add_product(product)
+#     await es.get_products(product.product_name)
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(main())
