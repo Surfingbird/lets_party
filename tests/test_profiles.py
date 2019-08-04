33d0ba5d7f3f6a0c53325d0cb8ba986228425033
Surfingbird <a.baronskiy@mail.ru>
2019-08-04 15:33:07 +0300

    тест на проверку получения желаний пользователя

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -151,16 +151,25 @@ async def test_del_intention_success(cli, cookie_pid_destid_prof_with_wish):
     data = await response.json()
     assert len(data['intentions']) == 0
 
-async def get_profiles_wishes_success(cli, valid_cookie, profile_vkid_with_wish_and_prod_id):
+async def test_get_profiles_wishes_success(cli, valid_cookie, profile_vkid_with_wish_and_prod_id):
     dest_id, _ = profile_vkid_with_wish_and_prod_id
 
-    response = await cli.get('/profile/' + dest_id + '/wishes', cookies=valid_cookie)
+    response = await cli.get('/profile/' + str(dest_id) + '/wishes', cookies=valid_cookie)
     assert response.status == 200
 
     data = await response.json()
     assert type(data) == list
     assert len(data) == 1
     extended_alien_wish_t.check(data[0])
+
+async def test_get_profiles_wishes_success_emtpy(cli, valid_cookie):
+    dest_id = str(random.randint(0, 100))
+
+    response = await cli.get('/profile/' + dest_id + '/wishes', cookies=valid_cookie)
+    assert response.status == 200
+
+    data = await response.json()
+    assert len(data) == 0
 
 
 
